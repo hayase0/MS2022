5bd5eba8a538ef510aba96a61f4eb53968c62fcc
鈴木 <hayase.github@gmail.com>
2021-10-21 10:40:18 +0900

    バグ修正

--- MS2022/source/model.cpp
+++ MS2022/source/model.cpp
@@ -386,8 +386,9 @@ void CModel::LoadMaterial(const char* FileName, MODEL_MATERIAL** MaterialArray, 
 
 
         if (strcmp(str, "newmtl") == 0) {
+            mc++;
             //マテリアル名
-            (void)fscanf(file, "%s", materialArray[mc + 1].Name);
+            (void)fscanf(file, "%s", materialArray[mc].Name);
             strcpy(materialArray[mc + 1].TextureName, "");
         }
         else if (strcmp(str, "Ka") == 0) {
@@ -421,7 +422,7 @@ void CModel::LoadMaterial(const char* FileName, MODEL_MATERIAL** MaterialArray, 
         }
         else if (strcmp(str, "map_Kd") == 0) {
             //テクスチャ
-            (void)(file, "%s", str);
+            (void)fscanf(file, "%s", str);
 
             char path[256];
             strcpy(path, "Assets/Models/");
