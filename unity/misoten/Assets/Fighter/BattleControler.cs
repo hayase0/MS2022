950e82ac11e27f3235bb35e82e1e0a6cbb4aee1b
鈴木 <hayase.github@gmail.com>
2022-02-12 16:11:11 +0900

    バトルフロー完成

--- unity/misoten/Assets/Fighter/BattleControler.cs
+++ unity/misoten/Assets/Fighter/BattleControler.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleControler : MonoBehaviour {
-    [SerializeField] GameObject fighter1, fighter2;
-    [SerializeField] int hp1, hp2;
-    [SerializeField] int maxhp = 100;
+public class BattleControler : SingletonMonoBehaviour<BattleControler> {
+    [SerializeField] GameObject prb_fighter1, prb_fighter2;    // ファイタープレハブ
+    [SerializeField] GameObject effect;                // スポーンエフェクト
+    [SerializeField] int hp1, hp2;                     // ファイターのHP
+    [SerializeField] int maxhp = 100;                  // ファイターの最大HP
 
-    Animator animator1, animator2;
-    bool move, damage;
+    GameObject fighter1, fighter2;                      // ファイターオブジェクト
+    Animator animator1, animator2;                     // ファイターのアニメーター
+    bool move, damage;                                 // ファイターの行動フラグ
+    bool play = false;
+    float timer = 0;                                   // タイマー
 
+
+    // バトルフローステート
     enum BATTLESTATE {
         SPAWN,
         WAIT,
@@ -19,22 +25,34 @@ public class BattleControler : MonoBehaviour {
     BATTLESTATE state;
 
     // Start is called before the first frame update
-    void Start() {
-        animator1 = fighter1.GetComponent<Animator>();
-        animator2 = fighter2.GetComponent<Animator>();
-        move = false;
-        state = BATTLESTATE.WAIT;
-
-        animator1.SetBool("Wait", true);
-        animator2.SetBool("Wait", true);
-
-        hp1 = hp2 = maxhp;
+    void Start() {       
+        state = BATTLESTATE.SPAWN;
+        fighter1 = null;
+        fighter2 = null;
     }
 
     // Update is called once per frame
     void Update() {
         switch (state) {
+            case BATTLESTATE.SPAWN:
+                move = false;
+
+                if (play == false) {
+                    // スポーンエフェクト再生
+                    Instantiate(effect, new Vector3(0.8f, 2.0f, 0.0f), Quaternion.identity);
+                    Instantiate(effect, new Vector3(-0.8f, 2.0f, 0.0f), Quaternion.identity);
+                    play = true;
+                }
+
+                if (fighter2 != null) {
+                    // 待機ステートに移行
+                    state = BATTLESTATE.WAIT;
+                    play = false;
+                }
+                break;
             case BATTLESTATE.WAIT:
+
+                // ファイトステートに移行
                 if (Input.GetKey(KeyCode.R)) {
                     animator1.SetBool("Wait", false);
                     animator2.SetBool("Wait", false);
@@ -124,10 +142,73 @@ public class BattleControler : MonoBehaviour {
                         move = false;
                     }
                 }
+
+                if(hp1 == 0 || hp2 == 0) {
+                    timer += Time.deltaTime;
+
+                    if (timer >= 5) {
+                        state = BATTLESTATE.END;
+                        timer = 0;
+                    }
+                }
                 break;
             case BATTLESTATE.END:
+                if (play == false) {
+                    Vector3 pos1, pos2;
+                    pos1 = fighter1.transform.position;
+                    pos2 = fighter2.transform.position;
+
+                    if (hp1 == 0) {
+                        pos1.y += 1.0f;
+                        pos2.y += 1.0f;
+                    }
+                    else {
+                        pos1.y += 1.0f;
+                        pos2.y += 1.0f;
+                    }
+
+                    // スポーンエフェクト再生
+                    Instantiate(effect, pos1, Quaternion.identity);
+                    Instantiate(effect, pos2, Quaternion.identity);
+                    play = true;
+                }
+
+                if(fighter2 == null) {
+                    // ファイトステートに移行
+                    if (Input.GetKey(KeyCode.R)) {                       
+                        state = BATTLESTATE.SPAWN;
+                        play = false;
+                    }
+                }
                 break;
 
         }       
+    }
+
+    public void CreateorDestroy() {
+        if (fighter1 == null && fighter2 == null) {
+            // ファイター二体を生成
+            fighter1 = Instantiate(prb_fighter1, new Vector3(0.8f, 1.0f, 0.0f), Quaternion.Euler(0.0f, -90.0f, 0.0f));
+            // アニメーター取得
+            animator1 = fighter1.GetComponent<Animator>();
+            // 待機モーションtrue
+            animator1.SetBool("Wait", true);
+            // HP初期化
+            hp1 = hp2 = maxhp;
+
+        }
+        else if(fighter2 == null) {
+            fighter2 = Instantiate(prb_fighter2, new Vector3(-0.8f, 1.0f, 0.0f), Quaternion.Euler(0.0f, 90.0f, 0.0f));
+            animator2 = fighter2.GetComponent<Animator>();
+            animator2.SetBool("Wait", true);
+        }
+        else if (fighter1 != null){
+            Destroy(fighter1);
+            fighter1 = null;
+        }
+        else {
+            Destroy(fighter2);
+            fighter2 = null;
+        }
     }
 }
