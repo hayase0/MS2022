bd6dbabde2650eef364414a8e86147221427ad0d
DESKTOP-E4ACP6J\Haruki <n.haruki990402@gmail.com>
2022-02-17 14:46:41 +0900

    タイトル追加

---
+++ unity/misoten/Assets/Title/CameraTitle.cs
@@ -0,0 +1,41 @@
+﻿//=======================================================================
+// カメラ回転・追従
+//=======================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTitle : MonoBehaviour
+{
+    GameObject targetObj;
+    Vector3 targetPos;
+    private float rotate_speed=1.0f;
+
+  //  private float speed = 0.005f;
+
+    void Start()
+    {
+        targetObj = GameObject.Find("Target");
+        targetPos = targetObj.transform.position;
+    }
+
+    void Update()
+    {
+        // targetの移動量分、自分（カメラ）も移動する
+        transform.position += targetObj.transform.position - targetPos;
+        targetPos = targetObj.transform.position;
+
+
+        // キーボード用
+        float rotate_x = 0f;
+
+        rotate_x = rotate_speed;
+
+      //  float speed_x = speed;
+     //   this.gameObject.transform.Translate(speed_x, 0, 0);
+
+        // targetの位置のY軸を中心に、回転（公転）する
+        transform.RotateAround(targetPos, Vector3.up, rotate_x * Time.deltaTime * 20f);
+    }
+}
---
+++ unity/misoten/Assets/Title/LogInOut.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogInOut : MonoBehaviour
+{
+    public GameObject login;
+    public GameObject logout;
+    
+    int title = 0;
+
+    void Start()
+    {
+        login.SetActive(true);
+        logout.SetActive(true);
+        //login = GameObject.Find("Start");
+        // logout = GameObject.Find("End");
+
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            title = 1;
+
+            login.SetActive(true);
+            logout.SetActive(false);
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            title = 0;
+            
+            login.SetActive(false);
+            logout.SetActive(true);
+
+        }
+        if (title == 1)
+        {
+            gameObject.SetActive(true);
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SceneManager.LoadScene("main");
+            }
+
+        }
+        else if (title == 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Quit();
+            }
+        }
+
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+
+
+}
+
